8d8ab92a0b3c1a7fbf215dbd91c016aa513d9d4b
itsserbin <serbin.official@gmail.com>
2023-09-24 09:53:27 +0300

    some update

--- app/Http/Controllers/Api/V1/OrdersController.php
+++ app/Http/Controllers/Api/V1/OrdersController.php
@@ -40,7 +40,7 @@ class OrdersController extends BaseController
      */
     final public function create(Request $request): JsonResponse
     {
-        $params = json_decode($request->all());
+        $params = json_decode($request->all()[0]);
         var_dump($params);
 
         $client = $this->createOrUpdateClient($params);
