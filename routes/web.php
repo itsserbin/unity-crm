b8f95117795e81c8ab7f4f99a495966f1b6f0c2c
itsserbin <serbin.official@gmail.com>
2023-11-25 19:52:13 +0200

    up

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
