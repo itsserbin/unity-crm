a196ff8c30f478840b7f6f96fd967cd484770834
itsserbin <serbin.official@gmail.com>
2023-11-25 18:41:36 +0200

    up

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -10,6 +10,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class Subscription extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'start_date',
+        'end_date',
+    ];
+
     final public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
